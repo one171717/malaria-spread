ef232afab6f3966699adf079d6e4a5856301be5f
deepakarnani <ddkarnani@yahoo.com>
2018-09-18 20:11:29 -0400

    adding updated js files

---
+++ static/choro50abovedeaths.js
@@ -0,0 +1,26 @@
+Plotly.d3.csv('https://raw.githubusercontent.com/one171717/malaria-spread/master/static/Deaths_50_69.csv', function(err, rows){
+      function unpack(rows, key) {
+          return rows.map(function(row) { return row[key]; });
+      }
+
+    var data = [{
+        type: 'choropleth',
+        locationmode: 'country names',
+        locations: unpack(rows, 'country '),
+        z: unpack(rows, 'Deaths50-69'),
+        text: unpack(rows, 'country '),
+        autocolorscale: true
+    }];
+
+    var layout = {
+      title: 'Average reported deaths of Malaria (50 - 69) in the last decade',
+      geo: {
+          projection: {
+              type: 'robinson'
+          }
+      }
+    };
+
+    Plotly.plot(myDiv, data, layout, {showLink: false});
+
+});
