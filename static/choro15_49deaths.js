c66031daacc02ed1b9617d51ce48299c8d6a30e9
deepakarnani <ddkarnani@yahoo.com>
2018-09-18 20:34:04 -0400

    adding updated js files

--- static/choro15_49deaths.js
+++ static/choro15_49deaths.js
@@ -1,4 +1,4 @@
-Plotly.d3.csv('https://raw.githubusercontent.com/one171717/malaria-spread/master/static/Deaths_allages_V2.csv', function(err, rows){
+Plotly.d3.csv('https://raw.githubusercontent.com/one171717/malaria-spread/master/static/Deaths_15_49.csv', function(err, rows){
       function unpack(rows, key) {
           return rows.map(function(row) { return row[key]; });
       }
@@ -7,13 +7,13 @@ Plotly.d3.csv('https://raw.githubusercontent.com/one171717/malaria-spread/master
         type: 'choropleth',
         locationmode: 'country names',
         locations: unpack(rows, 'country '),
-        z: unpack(rows, 'Deaths_all_ages'),
+        z: unpack(rows, 'Deaths15-49'),
         text: unpack(rows, 'country '),
         autocolorscale: true
     }];
 
     var layout = {
-      title: 'Average reported deaths of Malaria (all ages) in the last decade',
+      title: 'Average reported deaths of Malaria (15 - 49) in the last decade',
       geo: {
           projection: {
               type: 'robinson'
--- static/choro5_14deaths.js
+++ static/choro5_14deaths.js
@@ -1,4 +1,4 @@
-Plotly.d3.csv('https://raw.githubusercontent.com/one171717/malaria-spread/master/static/Deaths_allages_V2.csv', function(err, rows){
+Plotly.d3.csv('https://raw.githubusercontent.com/one171717/malaria-spread/master/static/Deaths_5_14.csv', function(err, rows){
       function unpack(rows, key) {
           return rows.map(function(row) { return row[key]; });
       }
@@ -7,13 +7,13 @@ Plotly.d3.csv('https://raw.githubusercontent.com/one171717/malaria-spread/master
         type: 'choropleth',
         locationmode: 'country names',
         locations: unpack(rows, 'country '),
-        z: unpack(rows, 'Deaths_all_ages'),
+        z: unpack(rows, 'Deaths_5_14'),
         text: unpack(rows, 'country '),
         autocolorscale: true
     }];
 
     var layout = {
-      title: 'Average reported deaths of Malaria (all ages) in the last decade',
+      title: 'Average reported deaths of Malaria (5 - 14 ages) in the last decade',
       geo: {
           projection: {
               type: 'robinson'
